bc69ab46c05d916d4bc493bf81568e01eff13e08
Elias <eliaskiso4@gmail.com>
2023-06-14 20:53:21 -0300

    Atividade E2E

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,10 @@
+describe('teste aluno-online', () => {
+  it('Novo Requerimento', () => {
+    cy.visit('http://localhost:3000/requerimentos')
+    cy.get('.btn').click()
+    cy.get('#inputDisciplina').select('ADS030 - Manutenção de Software e DevOps')
+    cy.get('#inputProva').select('A2')
+    cy.get('#inputArgumentacao').type('Eu mereço tirar 10')
+    cy.get('.btn-danger')
+  })
+})
